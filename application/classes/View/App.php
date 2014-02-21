803ed0fcfdff8e4f910af075fe1a9471e04eb6d5
ziopod <ziopod@gmail.com>
2014-02-21 16:45:27 +0100

    Configuration du CSS et de la navigation

--- application/classes/View/App.php
+++ application/classes/View/App.php
@@ -1,4 +1,32 @@
 <?php
 
 class View_App extends Flatfile_View_App {
+
+	/**
+	* Feuilles de styles
+	**/
+	public function styles()
+	{
+		return array(
+			array(
+				'src'	=> $this->base_url() . 'css/style.css',
+				'media'	=> 'screen',
+			),
+		);
+	}
+
+	/**
+	* Navigation principale
+	**/
+	public function navigation()
+	{
+		return array(
+			array(
+				'url'		=> $this->base_url() . 'about',
+				'name'		=> __('About'),
+				'title' 	=> __('More information about Ziopod'),
+				'current'	=> Request::initial()->controller() === 'App' AND Request::initial()->param('slug')  === 'about',
+			),
+		);
+	}
 }
